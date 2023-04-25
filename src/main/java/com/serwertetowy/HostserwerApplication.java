7858794f518041a17345a7c8d514fb7b08e7d5fd
Upril <maciej.okon@pollub.edu.pl>
2023-04-25 11:08:39 +0200

    Initial Commit

---
+++ src/main/java/com/serwertetowy/HostserwerApplication.java
@@ -0,0 +1,13 @@
+package com.serwertetowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostserwerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostserwerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serwertetowy/HostserwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serwertetowy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostserwerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
