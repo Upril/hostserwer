9d1104edd1439e8bb911f86052159a3bcecfa2c5
Upril <maciej.okon@pollub.edu.pl>
2023-05-10 12:41:46 +0200

    Added Language Repository and Entity Added Hibernate Validator

---
+++ src/main/java/com/serwertetowy/repos/LanguageRepository.java
@@ -0,0 +1,8 @@
+package com.serwertetowy.repos;
+
+import com.serwertetowy.entities.Languages;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LanguageRepository extends JpaRepository<Languages, Integer> {
+
+}
