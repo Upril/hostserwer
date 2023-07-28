834358a10d56a3fbf31e24ffa5cdecd0f8b15607
Upril <maciej.okon@pollub.edu.pl>
2023-07-28 14:15:16 +0200

    Added RatingController method validation

--- src/main/java/com/serwertetowy/controllers/RatingController.java
+++ src/main/java/com/serwertetowy/controllers/RatingController.java
@@ -1,16 +1,24 @@
 package com.serwertetowy.controllers;
 
 import com.serwertetowy.entities.Rating;
+import com.serwertetowy.exceptions.RatingNotFoundException;
+import com.serwertetowy.exceptions.SeriesNotFoundException;
+import com.serwertetowy.exceptions.UserNotFoundException;
 import com.serwertetowy.services.RatingService;
 import com.serwertetowy.services.dto.RatingSummary;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/v1/ratings")
@@ -18,11 +26,37 @@ import java.util.List;
 public class RatingController {
     private RatingService ratingService;
     //record to simplify the post request
-    record PostRatingRequest(@Min(1) @NotNull Long userId, @Min(1) @NotNull Long seriesId,
-                             @Min(1) @Max(10) @NotNull short plotRating, @Min(1) @Max(10) @NotNull short musicRating, @Min(1) @Max(10) @NotNull short graphicsRating,
-                             @Min(1) @Max(10) @NotNull short charactersRating, @Min(1) @Max(10) @NotNull short generalRating){}
-    record PutRatingRequest(short plotRating, short musicRating, short graphicsRating,
-                            short charactersRating, short generalRating){}
+    record PostRatingRequest(@Min(1) @NotNull(message = "User id is mandatory") Long userId, @Min(1) @NotNull(message = "Series id is mandatory") Long seriesId,
+                             @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                             @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                             @NotNull short plotRating,
+                             @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                             @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                             @NotNull short musicRating,
+                             @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                             @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                             @NotNull short graphicsRating,
+                             @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                             @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                             @NotNull short charactersRating,
+                             @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                             @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                             @NotNull short generalRating){}
+    record PutRatingRequest(@Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                            @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                            @NotNull short plotRating,
+                            @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                            @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                            @NotNull short musicRating,
+                            @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                            @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                            @NotNull short graphicsRating,
+                            @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                            @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                            @NotNull short charactersRating,
+                            @Min(value = 1,message = "Values of ratings must be between 1 and 10")
+                            @Max(value = 10,message = "Values of ratings must be between 1 and 10")
+                            @NotNull short generalRating){}
     //saving ratings with post requests
     @PostMapping
     public ResponseEntity<Rating> saveRating(@RequestBody @Valid PostRatingRequest request){
@@ -58,5 +92,44 @@ public class RatingController {
     public ResponseEntity<RatingSummary> getRatingById(@PathVariable("id") @Min(1) Long id){
         return new ResponseEntity<>(ratingService.getRating(id), HttpStatus.OK);
     }
+
+    private Map<String,String> messageCreator(Exception ex){
+        Map<String,String> errors = new HashMap<>();
+        errors.put("error",ex.getMessage());
+        return errors;
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String,String> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String name = ((FieldError) error).getField();
+            String msg = error.getDefaultMessage();
+            errors.put(name,msg);
+        });
+        return errors;
+    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Map<String,String> handleConstraintExceptions(ConstraintViolationException ex){
+        Map<String,String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach((error) -> {
+            String name = String.valueOf(error.getPropertyPath());
+            String msg =  error.getMessage();
+            errors.put(name,msg);
+        });
+        return errors;
+    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public Map<String,String> handleUserNotFoundExceptions(UserNotFoundException ex){
+        return messageCreator(ex);
+    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(SeriesNotFoundException.class)
+    public Map<String,String> handleSeriesNotFoundExceptions(SeriesNotFoundException ex){return messageCreator(ex);}
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(RatingNotFoundException.class)
+    public Map<String,String> handleRatingNotFoundExceptions(RatingNotFoundException ex){return messageCreator(ex);}
 
 }
