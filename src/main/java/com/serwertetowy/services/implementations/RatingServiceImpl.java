30845e7c24d5c982b42b8de37b28120199c3fb2f
Upril <maciej.okon@pollub.edu.pl>
2023-08-29 13:27:16 +0200

    Small refactor of RatingServiceImpl

--- src/main/java/com/serwertetowy/services/implementations/RatingServiceImpl.java
+++ src/main/java/com/serwertetowy/services/implementations/RatingServiceImpl.java
@@ -11,12 +11,10 @@ import com.serwertetowy.repos.RatingRepository;
 import com.serwertetowy.repos.SeriesRepository;
 import com.serwertetowy.repos.UserRepository;
 import com.serwertetowy.services.RatingService;
-import com.serwertetowy.services.dto.RatingSummary;
 import com.serwertetowy.services.UserService;
+import com.serwertetowy.services.dto.RatingSummary;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Objects;
@@ -24,10 +22,10 @@ import java.util.Objects;
 @Service
 @AllArgsConstructor
 public class RatingServiceImpl implements RatingService {
-    private RatingRepository ratingRepository;
-    private UserService userService;
-    private SeriesRepository seriesRepository;
-    private UserRepository userRepository;
+    private final RatingRepository ratingRepository;
+    private final UserService userService;
+    private final SeriesRepository seriesRepository;
+    private final UserRepository userRepository;
     @Override
     public RatingSummary getRating(Long id) {
         return ratingRepository.findById(id);
@@ -35,52 +33,72 @@ public class RatingServiceImpl implements RatingService {
 
     @Override
     public RatingSummary putRating(Long id, short plotRating, short musicRating, short graphicsRating, short charactersRating, short generalRating, String auth) {
-        if(!ratingRepository.existsById(id.intValue())) throw new RatingNotFoundException();
+
         Rating rating = ratingRepository.findById(id.intValue())
-                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(RatingNotFoundException::new);
+
         User user = rating.getUser();
-        if(!Objects.equals(user.getEmail(), auth)) throw new FailedToAuthenticateException();
+        if(!Objects.equals(user.getEmail(), auth)){
+            throw new FailedToAuthenticateException();
+        }
+
         rating.setPlotRating(plotRating);
         rating.setMusicRating(musicRating);
         rating.setGraphicsRating(graphicsRating);
         rating.setCharactersRating(charactersRating);
         rating.setGeneralRating(generalRating);
         ratingRepository.save(rating);
+
         return ratingRepository.findById(id);
     }
 
     @Override
     public void deleteRatingById(Long id, String auth) {
-        if(!ratingRepository.existsById(id.intValue())) throw new RatingNotFoundException();
+
         Rating rating = ratingRepository.findById(id.intValue())
-                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .orElseThrow(RatingNotFoundException::new);
+
         User user = rating.getUser();
-        if(!Objects.equals(user.getEmail(), auth)) throw new FailedToAuthenticateException();
+        if(!Objects.equals(user.getEmail(), auth)){
+            throw new FailedToAuthenticateException();
+        }
         ratingRepository.delete(rating);
     }
 
     @Override
     public List<RatingSummary> getRatingsBySeries(Long seriesId) {
-        if(!seriesRepository.existsById(seriesId.intValue())) throw new SeriesNotFoundException();
+        if(!seriesRepository.existsById(seriesId.intValue())){
+            throw new SeriesNotFoundException();
+        }
         return ratingRepository.findBySeriesId(seriesId);
     }
 
     @Override
     public List<RatingSummary> getRatingsByUser(Long userId) {
-        if(!userRepository.existsById(userId)) throw new UserNotFoundException();
+        if (!userRepository.existsById(userId)){
+            throw new UserNotFoundException();
+        }
         return ratingRepository.findByUserId(userId);
     }
 
     @Override
     public void saveRating(Long userId, Long seriesId, short plotRating, short musicRating, short graphicsRating, short charactersRating, short generalRating, String auth) {
-        if(!userRepository.existsById(userId)) throw new UserNotFoundException();
-        if(!seriesRepository.existsById(seriesId.intValue())) throw new SeriesNotFoundException();
+        if (!userRepository.existsById(userId)){
+            throw new UserNotFoundException();
+        }
+
+        if (!seriesRepository.existsById(seriesId.intValue())){
+            throw new SeriesNotFoundException();
+        }
+
         User user = userService.getUserById(userId);
-        if(!Objects.equals(user.getEmail(), auth)) throw new FailedToAuthenticateException();
+        if (!Objects.equals(user.getEmail(), auth)){
+            throw new FailedToAuthenticateException();
+        }
         //assemble rating data - get series information
         Series series = seriesRepository.findById(seriesId.intValue())
-                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
-        Rating rating = new Rating(series,user,plotRating,musicRating,graphicsRating,charactersRating,generalRating);
+                .orElseThrow(SeriesNotFoundException::new);
+        Rating rating = new Rating(series, user, plotRating, musicRating, graphicsRating, charactersRating, generalRating);
         ratingRepository.save(rating);
     }
 }
